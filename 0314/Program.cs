ce9f4f79e1e5e1420c69f0fe28aa1a98731a68db
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:37:56 +0800

    加入專案檔案。

---
+++ 0314/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _0314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,i;
+            Console.WriteLine("請輸入一個數字");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if(num % 2 != 0 )
+            {
+                Console.WriteLine("Odd Wried");
+            }
+            else if(num < 5 )
+            {
+                Console.WriteLine("even Not Wried");
+            }
+            else if(num <= 20)
+            {
+                Console.WriteLine("even Wried");
+            }
+
+            for(i=0;i<10;i++)
+            {
+                Console.WriteLine(i);
+            }   
+
+
+        }
+    }
+}
